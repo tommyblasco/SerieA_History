9bd4140b423fd523d23bcf2fb9ecf872b3fdb663
me <sacchinit@gmail.com>
2025-02-08 19:15:47 +0100

    prova albo d'oro

--- Home.py
+++ Home.py
@@ -15,8 +15,9 @@ seas_list = sorted(set(storico['Stagione']),reverse=True) #lista stagioni uniche
 stagione_curr=str(seas_list[0])  #stagione corrente
 
 gen_al=albo(seas_list) #generazione albo d'oro
-st.dataframe(pd.DataFrame.from_dict(gen_al, orient='index', columns=["Vincitore", "url_Stemma"]).reset_index(drop=True, inplace=True))
-
+df = pd.DataFrame.from_dict(gen_al, orient='index', columns=["Vincitore", "url_Stemma"])
+df = df.reset_index(drop=True)
+st.dataframe(df)
 
 #st.header("Stagione attuale "+stagione_curr)
 #tm_delta=st.slider("Partite nei prossimi ... giorni",min_value=0,max_value=35,value=7,step=7)

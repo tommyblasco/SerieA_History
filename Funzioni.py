af551f1e66b4169a3a0a533f5b119902a37cfad5
me <sacchinit@gmail.com>
2025-02-08 17:19:37 +0100

    prova albo d'oro

--- Funzioni.py
+++ Funzioni.py
@@ -3,19 +3,28 @@ import numpy as np
 from datetime import date, datetime, timedelta
 import streamlit as st
 from github import Github
+from PIL import Image, ImageDraw, ImageFont
+from io import BytesIO
+import requests
 
 conn_g=Github(st.secrets['TOKEN'])
 repo_seriea=conn_g.get_user("tommyblasco").get_repo("SerieA_History")
 
-@st.cache
 def load_data(df):
     if df!='Penalizzazioni':
-        l_data = pd.read_csv("https://raw.githubusercontent.com/tommyblasco/SerieA_History/refs/heads/main/Dati/" + df + ".csv",
+        l_data = pd.read_csv(f"https://raw.githubusercontent.com/tommyblasco/SerieA_History/refs/heads/main/Dati/{df}.csv",
                              sep=",", decimal=".", parse_dates=['Data'], dayfirst=True)
     else:
-        l_data = pd.read_csv("https://raw.githubusercontent.com/tommyblasco/SerieA_History/refs/heads/main/Dati/" + df + ".csv",
+        l_data = pd.read_csv(f"https://raw.githubusercontent.com/tommyblasco/SerieA_History/refs/heads/main/Dati/{df}.csv",
                              sep=",", decimal=".", parse_dates=['Da','A'], dayfirst=True)
     return l_data
+
+def load_images(team,yyyy):
+    stemmi_ava=repo_seriea.get_contents(f"images/stemmi/{team}/")
+    file_names = [file.name.split(".")[0] for file in stemmi_ava]
+    yy_sel=min([x for x in file_names if x>=yyyy])
+    url_stemma=f"https://raw.githubusercontent.com/tommyblasco/SerieA_History/tree/main/images/stemmi/{team}/{yy_sel}.png".replace(' ','%20')
+    return url_stemma
 
 storico=load_data("Partite")
 tbd=load_data("TBD")
@@ -51,16 +60,27 @@ def ranking(seas,st_date=datetime(1900,1,1),en_date=datetime.now()):
     else:
         new_class=classifica[['Squadra','Gio','V','N','P','GF','GS']]
         new_class = new_class.sort_values(by=['Gio'], ascending=False)
+    new_class.insert(0,'Rk',range(1,new_class.shape[0]+1))
     return new_class
 
+#gen vincitori
+def albo(sl):
+    vin, stem = [],[]
+    for s in sl:
+        cl_se=ranking(seas=s)
+        team_win=cl_se.iloc[0,1]
+        stem.append(load_images(team_win,s[:4]))
+        vin.append(team_win)
+    return {k:[v1,v2] for k,v1,v2 in zip(sl,vin,stem)}
+
+#prossima giornata avendo anche il rif alla posizione della squadra
 def nx_match_rank(s,n):
-    cur_rank = ranking(seas=s)[['Squadra']]
-    cur_rank['rank'] = range(1, len(cur_rank) + 1)
+    team_list = ranking(seas=s)[['Rk','Squadra']]
     prox_partite = tbd[tbd['Data']<datetime.now()+timedelta(days=n)][['Giornata','Data','CASA','TRAS']]
 
-    rank_h = prox_partite.merge(cur_rank, left_on='CASA', right_on='Squadra', how='left').drop('Squadra', axis=1)
+    rank_h = prox_partite.merge(team_list, left_on='CASA', right_on='Squadra', how='left').drop('Squadra', axis=1)
     rank_h.rename(columns={'rank': 'rank_h'},inplace=True)
-    rank_a = rank_h.merge(cur_rank, left_on='TRAS', right_on='Squadra', how='left').drop('Squadra', axis=1)
+    rank_a = rank_h.merge(team_list, left_on='TRAS', right_on='Squadra', how='left').drop('Squadra', axis=1)
     rank_a.rename(columns={'rank': 'rank_a'},inplace=True)
     rank_a['Home'] = [x + ' (' + str(y) + '.)' for x, y in zip(rank_a['CASA'], rank_a['rank_h'])]
     rank_a['Away'] = [x + ' (' + str(y) + '.)' for x, y in zip(rank_a['TRAS'], rank_a['rank_a'])]
--- Home.py
+++ Home.py
@@ -6,7 +6,13 @@ st.title("Serie A")
 
 st.subheader("Database storico della Serie A a girone unico")
 
-stagione_curr=str(max(set(storico['Stagione'])))
+seas_list = sorted(set(storico['Stagione']),reverse=True)
+stagione_curr=str(seas_list[0])
+
+st.image(Image.open(BytesIO(requests.get(load_images(team='JUVENTUS',yyyy='2015')).content)), caption='JUVENTUS',
+         use_column_width=True)
+
+
 st.header("Stagione attuale "+stagione_curr)
 tm_delta=st.slider("Partite nei prossimi ... giorni",min_value=0,max_value=35,value=7,step=7)
 

a5f70b1e44acb704c027074f29de62f13afaa76f
me <sacchinit@gmail.com>
2025-02-08 19:40:17 +0100

    prova albo d'oro

--- Home.py
+++ Home.py
@@ -18,7 +18,7 @@ for i in range(0, len(albo), 10):
     cols = st.columns(10)
     for j, (_, row) in enumerate(albo.iloc[i:i + 10].iterrows()):
         with cols[j]:
-            st.image(Image.open(BytesIO(requests.get(row['url_Stemma']).content)), caption=f"{row['Vincitore']} ({row['Stagione']})", use_column_width=True)
+            st.image(Image.open(BytesIO(requests.get(row['url_Stemma']).content)), caption=f"{row['Vincitore']} ({row['Stagione']})", use_container_width=True)
 
 #st.header("Stagione attuale "+stagione_curr)
 #tm_delta=st.slider("Partite nei prossimi ... giorni",min_value=0,max_value=35,value=7,step=7)

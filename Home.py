58f3513f693709026282ecc385cfb8c410fcd116
me <sacchinit@gmail.com>
2025-02-09 11:32:33 +0100

    home finished

--- Home.py
+++ Home.py
@@ -31,6 +31,7 @@ with rpl:
     riep_grp = riep_grp.sort_values(by='Stagioni')
     part_gr = go.Figure()
     part_gr.add_trace(go.Bar(y=riep_grp['Squadre'], x=riep_grp['Stagioni'], orientation='h'))
+    part_gr.update_layout(height=800)
     st.plotly_chart(go.FigureWidget(data=part_gr))
 
 with part:

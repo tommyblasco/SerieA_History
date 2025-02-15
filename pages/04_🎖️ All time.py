db752d4fee5eb1064bd18c7b6410321a6129cee5
me <sacchinit@gmail.com>
2025-02-15 17:44:09 +0100

    alltime added

--- pages/04_🎖️ All time.py
+++ pages/04_🎖️ All time.py
@@ -2,7 +2,7 @@ import streamlit as st
 st.set_page_config(page_title="Serie A - All timw",layout='wide')
 from Funzioni import *
 
-perp = st.tabs(['Classifica perpetua'])
+perp, rec = st.tabs(['Classifica perpetua','Record'])
 
 with perp:
     pclass=ranking(seas='All').drop('Rk',axis=1)

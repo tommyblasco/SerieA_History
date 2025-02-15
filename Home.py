5077203f5ffda99225e83a3fd54608638a050384
me <sacchinit@gmail.com>
2025-02-15 17:48:15 +0100

    alltime added

--- Home.py
+++ Home.py
@@ -12,7 +12,7 @@ stagione_curr=str(seas_list[0])  #stagione corrente
 
 rpl, part = st.columns(2)
 with rpl:
-    st.text('Partecipazioni Serie A')
+    st.subheader('Partecipazioni Serie A')
     part_gr = go.Figure()
     part_gr.add_trace(go.Bar(y=riep_grp['Squadre'], x=riep_grp['Stagioni'],orientation='h'))
     part_gr.update_layout(height=1200)

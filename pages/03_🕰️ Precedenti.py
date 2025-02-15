dd2be1f4c29e899a8e277e36ae1d1b029073d970
me <sacchinit@gmail.com>
2025-02-15 12:06:18 +0100

    precedenti added

--- pages/03_🕰️ Precedenti.py
+++ pages/03_🕰️ Precedenti.py
@@ -45,7 +45,9 @@ with h2h:
             beta=((df1['WH'].item()+df2['WA'].item())/(df2['WH'].item()+df1['WA'].item()))-1
             fig.add_trace(go.Scatter(x=[-2,0,2], y= np.asarray([-2,0,2])*beta))
             fig.add_trace(go.Scatter(x=[-2,2], y=[(-2*beta)+0.1, (2*beta)+0.1], mode='markers',
-                                     marker=dict(color=['orange','blue'],size=[df1['WH'].item()+df2['WA'].item(),df2['WH'].item()+df1['WA'].item()])))
+                                     marker=dict(name=['W T1','W T2'],color=['orange','blue'],size=[df1['WH'].item()+df2['WA'].item(),df2['WH'].item()+df1['WA'].item()])))
+            fig.update_layout(xaxis=dict(showgrid=False,showticklabels=False), yaxis=dict(range=[-2,2],showgrid=False,showticklabels=False))
+
             st.plotly_chart(fig, use_container_width=True)
 
             wh_d_wa3 = go.Pie(hole=0.5, sort=False, direction='clockwise', values=[df1['WH'].item()+df2['WA'].item(), df1['N'].item()+df2['N'].item() ,df1['WA'].item()+df2['WH'].item()],

9ff33adce93e08764f979b807b7a924fdebb1a5b
Thomas <sacchinit@gmail.com>
2025-02-25 15:50:28 +0100

    Update 01_📅 Stagioni.py

--- pages/01_📅 Stagioni.py
+++ pages/01_📅 Stagioni.py
@@ -10,6 +10,7 @@ with c2:
     if st.button("Aggiorna Dati"):
         st.session_state.storico = load_data(n="gspartite")
         st.session_state.marcatori = load_data(n="gsmarcatori")
+        st.rerun()
 
 df=storico[storico['Stagione']==sea_sel]
 start_sea=min(df['Data'])

d4376fbb7610fc5b55ef85d94f85a88b7f38cbef
me <sacchinit@gmail.com>
2026-01-24 22:00:58 +0100

    form marcatori

--- Home.py
+++ Home.py
@@ -130,7 +130,7 @@ with st.expander("Updates partite"):
 with (st.expander("Updates marcatori")):
     st.text("Info generali")
     col1, col2, col3 = st.columns(3)
-    mnew_stag = col1.text_input("Stagione",key='stag_marc')
+    mnew_stag = col1.text_input("Stagione","0000",key='stag_marc')
     mnew_data = col2.date_input("Data",key='data_marc')
     mnew_gio = col3.number_input("Giornata", min_value=1, max_value=38, step=1,key='gio_marc')
 
